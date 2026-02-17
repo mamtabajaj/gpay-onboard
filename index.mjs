e35fbf87d8cfba6b9a5d06901eac61a7eed11cf6
Mamta Bajaj <mamta.bajaj8@gmail.com>
2026-02-17 18:36:33 -0500

    merge to development

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda here '),
+    body: JSON.stringify('I am using development branch'),
   };
   return response;
 };
