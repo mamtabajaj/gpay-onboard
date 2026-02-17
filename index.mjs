2f65e3ff52e5220529dbeb4905c6f1ed51e98a62
Mamta Bajaj <mamta.bajaj8@gmail.com>
2026-02-17 18:34:55 -0500

    changes deploy to development branch

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda here automatic workflow using deploy.yml'),
+    body: JSON.stringify('Hello from Lambda here '),
   };
   return response;
 };
