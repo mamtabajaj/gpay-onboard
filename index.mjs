ba2b42a4a26e9dd15da69409f8cee65518c2d1b2
Mamta Bajaj <mamta.bajaj8@gmail.com>
2026-02-17 18:29:39 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda here automatic workflow'),
+    body: JSON.stringify('Hello from Lambda here automatic workflow using deploy.yml'),
   };
   return response;
 };
